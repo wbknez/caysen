fc4cb15d5518cf664e0515f7279add47f3f6a1bd
Will Knez <wbknez.dev@gmail.com>
2017-08-02 18:45:00 -0600

    Add application entry point and package initializer.

---
+++ caysen/main.py
@@ -0,0 +1,19 @@
+"""
+The main driver for Caysen, a small, example roguelike village that is used
+to explore artificial intelligence algorithms that emphasize emotional
+awareness and social cognition.
+"""
+import sys
+
+
+def main():
+    """
+    The application entry point.
+
+    :return: An exit code.
+    """
+    pass
+
+
+if __name__ == "__main__":
+    sys.exit(main())
