e720bed09cc87cef139733e66a9f1544efd962bc
Will Knez <wbknez.dev@gmail.com>
2017-08-11 01:36:00 -0600

    Add initial (incomplete) version of the kernel.

--- caysen/systems.py
+++ caysen/systems.py
@@ -4,7 +4,7 @@ kernel to manage them.
 """
 from abc import ABCMeta, abstractmethod
 
-import tdl
+from caysen.util.timers import SystemTimer
 
 
 class SubSystem(metaclass=ABCMeta):
@@ -26,8 +26,8 @@ class SubSystem(metaclass=ABCMeta):
         both initialization and updates.
 
         Put more clearly, every dependency for each execution - either
-        one-time initialization or repeated updates - must be initialized or
-        updated before this sub-system may do the same.
+        one-time initialization, repeated updates, or shutdown - must be
+        initialized or updated before this sub-system may do the same.
 
         :return: A dict of initialization and update dependencies.
         """
@@ -66,157 +66,130 @@ class SubSystem(metaclass=ABCMeta):
         pass
 
 
-class DisplaySubSystem(SubSystem):
+def _get_execution_order(deps):
     """
-    An implementation of SubSystem that manages a console window that is used as
-    an ASCII-capable rendering surface.
+    Resolves the specified dictionary of dependencies into a flat list,
+    organized from those with the least dependencies to those with the most
+    in order to ensure all sub-systems execute before their dependants.
 
-    To prevent tearing, this implementation uses an additional offscreen
-    console that is, once per frame, blit to the main window via an update
-    function.
+    This algorithm was taken from a StackOverflow question at the following
+    link:
+    https://stackoverflow.com/questions/5287516/dependencies-tree-implementation
+    and the link to the original script in Python is:
+        https://code.activestate.com/recipes/576570-dependency-resolver/
+
+    :param deps: A dictionary of sub-system names with associated dependencies.
+    """
+    tree = []
+
+    while deps:
+        # Dependencies that are not named in the original dict.
+        # This does not actually happen in this project but it is still
+        # an important case to cover.
+        no_deps = set(subsystem for dep in deps.values() \
+                      for subsystem in deps) - set(deps.keys())
+
+        # Named dependants with no associated dependencies.
+        no_deps.update(subsystem for subsystem, dep in deps.items()
+                       if not dep)
+
+        # Because they have no further dependencies, they can be added
+        # to the resulting dependency tree.
+        tree.append(no_deps)
+
+        # Remove dependencies that have been met (are in the tree).
+        deps = dict(((subsystem, dep - no_deps)
+                     for subsystem, dep in deps.items() if dep))
+
+    # Flatten.
+    return [item for subtree in tree for item in subtree]
+
+
+def _get_dependencies(for_state, subsystems):
+    """
+
+    :param for_state:
+    :return:
+    """
+    deps = dict()
+
+    for subsystem in subsystems.values():
+        deps[subsystem.name] = subsystem.get_dependencies()[for_state]
+    return deps
+
+
+class Kernel:
+    """
+
 
     Attributes:
-        back (tdl.Console): The back buffer used as an offscreen display; all
-        rendering calls are written here before being blitted to the main
-        display.
-        console (tdl.Console): Main display.
-        font (str): Path to the bitmap font used for text presentation on a
-        console.
-        fps (int): The frames per second to limit the display to.
-        fullscreen (bool): Whether or not the display is consumes the entire
-        screen.
-        height (int): The number of rows in the display console.
-        title (str): The title to use for the display console.
-        width (int): The number of columns in the display console.
+        exec_order (list):
+        is_running (bool):
     """
 
     def __init__(self):
-        super().__init__("display")
-        self.back = None
-        self.console = None
-        self.font = None
-        self.fps = 20
-        self.fullscreen = False
-        self.height = 0
-        self.title = ""
-        self.width = 0
+        self.exec_order = list()
+        self.is_running = False
+        self.subsystems = dict()
+        self.timer = SystemTimer()
 
-    def clear(self, x, y):
+    def add(self, system, alternate_name=None):
         """
-        Clears the character located at the specified x- and y-axis
-        coordinates by, essentially, inserting a space in its place.
 
-        Note that this is applied to the back buffer, so a call to update()
-        must occur before these changes are visualized to the user.
-
-        :param x: The x-axis coordinate of the tile to clear.
-        :param y: The y-axis coordinate of the tile to clear.
+        :param system:
+        :param alternate_name:
+        :return:
         """
-        self.back.draw_char(x, y, ' ', bg=None)
-
-    def clear_all(self):
-        """
-        Clears the entire visual display.
-
-        Note that this is applied to the back buffer, so a call to update()
-        must occur before these changes are visualized to the user.
-        """
-        self.back.clear()
-
-    def draw(self, x, y, char, fg=(255, 255, 255), bg=None):
-        """
-        Draws the specified character to the screen at the specified
-        x- and y-axis coordinates and with the specified foreground and
-        background colors.
-
-        Note that this is applied to the back buffer, so a call to update()
-        must occur before these changes are visualized to the user.
-
-        :param x: The x-axis coordinate of the tile to draw on.
-        :param y: The y-axis coordinate of the tile to draw on.
-        :param char: The ASCII symbol to use as the foreground character.
-        :param fg: The foreground color.
-        :param bg: The background color.
-        """
-        self.back.draw_char(x, y, char, fg, bg)
-
-    def get_dependencies(self):
-        return {"init": [], "update": ["input"]}
+        if alternate_name is None:
+            other_name = system.name
+        self.subsystems[alternate_name] = system
 
     def initialize(self, params):
-        self.fullscreen = params.get("fullscreen", False)
-        self.font = params.get("font", None)
-        self.height = params.get("height", 50)
-        self.title = params.get("title", "Caysen City")
-        self.width = params.get("width", 80)
+        self._set_execution_order("init")
+        for subsystem in self.exec_order:
+            subsystem.initialize(params)
 
-        if self.font is not None:
-            greyscale = params.get("font.greyscale", True)
-            alt_layout = params.get("font.altLayout", False)
+    def remove(self, name):
+        if self.subsystems[name]:
+            del self.subsystems[name]
 
-            tdl.set_font(self.font, greyscale, alt_layout)
-
-        self.back = tdl.Console(self.width, self.height)
-        self.console = tdl.init(self.width, self.height, title=self.title,
-                                fullscreen=self.fullscreen)
-        tdl.set_fps(self.fps)
-        return self.console is not None
-
-    def set_fullscreen(self, fullscreen):
+    def run(self):
         """
-        Sets whether or not to make the display fullscreen.
 
-        :param fullscreen: Whether or not to make the display fullscreen.
+        :return:
+        :raise ValueError: If the kernel is already running or there are no
+        sub-systems available for use.
         """
-        if self.fullscreen is not fullscreen:
-            self.fullscreen = fullscreen
-            tdl.set_fullscreen(self.fullscreen)
+        if self.is_running:
+            raise ValueError('Kernel is already running.')
+
+        if not self.subsystems:
+            raise ValueError('There are no sub-systems available for use.')
+
+        self._set_execution_order("update")
+        self.is_running = True
+        self.timer.start()
+
+        while self.is_running:
+            self.timer.update()
+            for subsystem in self.exec_order:
+                if not subsystem.update(self.timer.delta_time):
+                    self.is_running = False
+                    break
+
+    def _set_execution_order(self, for_state):
+        """
+
+        :param for_state:
+        """
+        deps = _get_dependencies(for_state, self.subsystems)
+        exec_names = _get_execution_order(deps)
+
+        self.exec_order.clear()
+        for name in exec_names:
+            self.exec_order.append(self.subsystems[name])
 
     def shutdown(self):
-        if self.console is not None:
-            del self.console
-        return True
-
-    def update(self, delta_time):
-        self.console.blit(self.back, 0, 0, self.width, self.height, 0, 0)
-        tdl.flush()
-        return not tdl.event.is_window_closed()
-
-
-class InputSubSystem(SubSystem):
-    """
-    An implementation of SubSystem that manages the event queue of keyboard
-    and mouse input events and responds to each by either calling a
-    user-created callback function, if bound, otherwise discarding it.
-
-    Attributes::
-
-    """
-
-    def __init__(self):
-        super().__init__("input")
-
-    def get_dependencies(self):
-        return {"init": ["display"], "update": []}
-
-    def initialize(self, params):
-        pass
-
-    def shutdown(self):
-        pass
-
-    def update(self, delta_time):
-        for event in tdl.event.get():
-            if event.type == 'KEYDOWN':
-                pass
-            elif event.type == 'KEYUP':
-                pass
-            elif event.type == 'MOUSEDOWN':
-                pass
-            elif event.type == 'MOUSEMOTION':
-                pass
-            elif event.type == 'MOUSEUP':
-                pass
-            elif event.type == 'QUIT':
-                return False
-        return True
+        self._set_execution_order("shutdown")
+        for subsystem in self.exec_order:
+            subsystem.shutdown()
