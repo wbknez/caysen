8f949861a52c657d73b7a993f9a8b9bbe105b841
Will Knez <wbknez.dev@gmail.com>
2017-08-21 08:16:52 -0600

    Add skeleton for audio subsystem.

--- caysen/subsystem/audio.py
+++ caysen/subsystem/audio.py
@@ -1,3 +1,25 @@
 """
 Contains the implementation of the audio framework using PyDub.
 """
+from caysen.kernel import SubSystem
+
+
+class AudioSubSystem(SubSystem):
+    """
+
+    """
+
+    def __init__(self):
+        super().__init__("audio")
+
+    def get_dependencies(self):
+        pass
+
+    def initialize(self, params, kernel):
+        pass
+
+    def shutdown(self):
+        pass
+
+    def update(self, delta_time):
+        pass
