e217c42944314e9902515b3a4187f19e0efa1e9d
Will Knez <wbknez.dev@gmail.com>
2017-08-21 08:17:49 -0600

    Add game state module and add skeleton of game subsystem.

---
+++ caysen/game/state.py
@@ -0,0 +1,25 @@
+"""
+
+"""
+from caysen.kernel import SubSystem
+
+
+class GameSubSystem(SubSystem):
+    """
+
+    """
+
+    def __init__(self):
+        super().__init__("game")
+
+    def get_dependencies(self):
+        pass
+
+    def initialize(self, params, kernel):
+        pass
+
+    def shutdown(self):
+        pass
+
+    def update(self, delta_time):
+        pass
